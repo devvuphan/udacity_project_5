ea576d905d2b9deab241455eba10b13fc7398ff3
Vu Phan <phananhvuk5@gmail.com>
2022-06-22 11:34:59 +0700

    Init source

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'm85tnf9abc'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-700fbzt1.us.auth0.com',            // Auth0 domain
+  clientId: 'F8r94zg1L5fqrzmF7v3g2plchEbmIr00',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
